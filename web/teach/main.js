67dd249e00706ab51b128fd2ed36f870bdbd5318
Brady Koehler <brady.koehler@administr.com>
2019-01-16 19:49:25 -0700

    Core requirements

---
+++ web/teach/main.js
@@ -0,0 +1,8 @@
+function buttonClick() {
+  alert("Clicked!");
+}
+
+function changeColor() {
+  var color = document.getElementById("color").value;
+  document.getElementById("one").style.backgroundColor = color;
+}
