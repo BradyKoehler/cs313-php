f4e971a39cb15a0bd3332a8d620cf9d715676376
Brady Koehler <brady.koehler@administr.com>
2019-01-16 20:00:12 -0700

    Add fading div

--- web/teach/main.js
+++ web/teach/main.js
@@ -7,4 +7,16 @@ $(function() {
     var color = $("#color").val();
     $("#one").css("background-color", color);
   });
+
+  $("#goaway").click(function() {
+    $("#three").fadeOut(500);
+    $(this).hide();
+    $("#comeback").show();
+  });
+
+  $("#comeback").click(function() {
+    $("#three").fadeIn(500);
+    $(this).hide();
+    $("#goaway").show();
+  });
 });
