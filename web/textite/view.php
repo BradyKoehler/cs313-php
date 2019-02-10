8319bc0b07b5e69a4e8ba1351ad1a78c98f88246
Brady Koehler <brady.koehler@administr.com>
2019-02-09 22:25:11 -0700

    commit

--- web/textite/view.php
+++ web/textite/view.php
@@ -15,7 +15,7 @@ $text = $result->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <div class="note-view">
-  <p class="name"><?= $text['note']; ?></p>
+  <p class="name"><?= $text['name']; ?></p>
   <p>
     <span class="date"><?= $text['created_at']; ?></span>
     <span class="size"><?= strlen($text['content']); ?></span>
