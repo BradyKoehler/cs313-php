a0e2efc7e45c18d2ff4762116fef329ea59f2ae8
Brady Koehler <brady.koehler@administr.com>
2019-02-09 22:24:41 -0700

    commit

--- web/textite/view.php
+++ web/textite/view.php
@@ -11,43 +11,17 @@ require('shared.php');
 
 <?php
 $result = $db->query('SELECT * FROM texts WHERE id=1');
-$row = $result->fetch(PDO::FETCH_ASSOC);
+$text = $result->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <div class="note-view">
-  <p class="name"></p>
-  <?= var_dump($result); ?><br /><br />
-  <?= var_dump($row); ?>
+  <p class="name"><?= $text['note']; ?></p>
   <p>
-    <span class="date">January 1, 2019</span>
-    <span class="size">150kb</span>
+    <span class="date"><?= $text['created_at']; ?></span>
+    <span class="size"><?= strlen($text['content']); ?></span>
   </p>
   <pre>
-    rwildcard=$(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2)$(filter $(subst *,%,$2),$d))
-    src = $(call rwildcard,src/,*.cpp *.c)
-    obj = $(patsubst %,obj/%.o,$(basename $(notdir $(src))))
-
-    $(info src: [$(src)])
-    $(info obj: [$(obj)])
-
-    game.exe: $(obj)
-        g++ $^ -o $@
-
-    define objFromSrc
-    $(1): $(2)
-        $(info $(1) $(2))
-        g++ -c $(2) -o $(1)
-    endef
-
-    $(foreach t,$(src),$(call objFromSrc,$(patsubst %,obj/%.o,$(basename $(notdir $(t)))),$(t)))
-
-    src: [src/dir/main.cpp src/dir/dir2/other3.cpp src/dir/other2.cpp src/other.c]
-    obj: [obj/main.o obj/other3.o obj/other2.o obj/other.o]
-    obj/main.o src/dir/main.cpp
-    obj/other3.o src/dir/dir2/other3.cpp
-    obj/other2.o src/dir/other2.cpp
-    obj/other.o src/other.c
-    makefile:20: *** multiple target patterns.  Stop.
+    <?= $text['content']; ?>
   </pre>
 </div>
 
