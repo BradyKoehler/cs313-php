1e092e72753f857c03ea8f4e8b63f12dc5d420a1
Brady Koehler <brady.koehler@administr.com>
2019-02-09 21:28:41 -0700

    commit

--- web/textite/index.php
+++ web/textite/index.php
@@ -35,8 +35,8 @@ require('shared.php');
 <?php
 
 foreach ($db->query('SELECT * FROM notes') as $note) {
-  print "<div class='note-link'><p class='name'>$note['name']</p>";
-  print "<p><span class='date'>$note['created_at']</span>";
+  print "<div class='note-link'><p class='name'>" . $note['name'] . "</p>";
+  print "<p><span class='date'>" . $note['created_at'] . "</span>";
   print "<span class='size'>" . strlen($note['text']) . "</span></p></div>";
 }
 
