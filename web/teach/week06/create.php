c105169c5a1aab8430ae15d698f821020e630729
Brady Koehler <me@bradykoehler.com>
2019-02-13 11:26:29 -0700

    commit

---
+++ web/teach/week06/create.php
@@ -0,0 +1,55 @@
+<?php
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex </p>\n\n";
+ die();
+}
+
+function sanitize($data) {
+  return htmlspecialchars(stripslashes(trim($data)));
+}
+
+$db->query("INSERT INTO scriptures (book, chapter, verse, content) VALUES (" .
+           sanitize($_POST['book']) . ", " .
+           sanitize($_POST['chapter']) . ", " .
+           sanitize($_POST['verse']) . ", " .
+           sanitize($_POST['content']) . ")"
+          );
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <p><?= var_dump($_POST); ?></p>
+
+<?php foreach ($db->query('SELECT * FROM scriptures') as $scripture): ?>
+  <p><?= $scripture["name"]; ?></p>
+  <ul>
+    <?php foreach ($db->query('SELECT t.id, t.name FROM scriptures s ' .
+                              'JOIN xrefs x on (s.id = x.scripture_id) JOIN topics t on (x.topic_id = t.id)') as $topic): ?>
+    <li><?= $topic["name"]; ?></li>
+    <?php endforeach; ?>
+  </ul>
+<?php endforeach; ?>
+
+</body>
+
+</html>
